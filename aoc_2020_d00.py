702ce5f354e0895f599b11945467048d9bd3ee76
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-04 18:42:59 +0100

    day01

---
+++ aoc_2020_d00.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from datetime import timedelta
+from collections import defaultdict, deque
+import copy
+import re
+import time
+
+# .\get.ps1 0
+
+start = datetime.now()
+lines = open('0.in').readlines()
+
+
+def solve1(lines):
+    res = 0
+    return res
+
+
+def solve2(lines):
+    res = 0
+    return res
+
+
+print(solve1(lines))  #
+# print(solve2(lines))  #
+
+stop = datetime.now()
+print("duration:", stop - start)
