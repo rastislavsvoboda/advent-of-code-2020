0aa81cba91882fe09eea1bd0524b27c61e5f3c2c
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-05 08:01:06 +0100

    day05

---
+++ aoc_2020_d05.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from datetime import timedelta
+from collections import defaultdict, deque
+import copy
+import re
+import time
+
+# .\get.ps1 5
+
+start = datetime.now()
+lines = open('5.in').readlines()
+
+
+def half(min_val, max_val, ch):
+    mid_val = (max_val - min_val + 1) // 2
+    if ch in ['F', 'L']:
+        # Front or Left reduces max
+        max_val = max_val - mid_val
+    elif ch in ['B', 'R']:
+        # Back or Right reduces min
+        min_val = min_val + mid_val
+    else:
+        assert False, "unknown char: " + ch
+    return (min_val, max_val)
+
+
+def get_seat_ids(lines):
+    seat_ids = []
+    for line in lines:
+        line = line.strip()
+
+        # first 7 chars: row from 0 - 127
+        min_row, max_row = 0, 127
+        for r in line[:7]:
+            (min_row, max_row) = half(min_row, max_row, r)
+        assert min_row == max_row, "wrong row"
+        row = min_row
+
+        # next char: columns from 0 - 7
+        min_col, max_col = 0, 7
+        for c in line[7:]:
+            (min_col, max_col) = half(min_col, max_col, c)
+        assert min_col == max_col, "wrong column"
+        col = min_col
+
+        # formula for seat_id
+        seat_id = row * 8 + col
+        seat_ids.append(seat_id)
+    return seat_ids
+
+
+def solve1(lines):
+    seat_ids = sorted(get_seat_ids(lines), reverse=True)
+    # max seat id
+    res = seat_ids[0]
+    return res
+
+
+def solve2(lines):
+    res = None
+    seat_ids = sorted(get_seat_ids(lines))
+    sid = seat_ids[0]
+    for i in seat_ids[1:]:
+        if i != sid + 1:
+            # missing next sid
+            res = sid + 1
+            break
+        sid = i
+    return res
+
+
+# def solve2b(lines):
+#     res = 0
+#     seat_ids = sorted(get_seat_ids(lines))
+#     for i in range(seat_ids[0], seat_ids[-1] + 1):
+#         if i not in seat_ids:
+#             res = i
+#             break
+#     return res
+
+
+print(solve1(lines))  # 896
+print(solve2(lines))  # 659
+
+stop = datetime.now()
+print("duration:", stop - start)
---
+++ aoc_2020_d05bpy
@@ -0,0 +1,39 @@
+from datetime import datetime
+from datetime import timedelta
+from collections import defaultdict, deque
+import copy
+import re
+import time
+
+# .\get.ps1 5
+
+start = datetime.now()
+lines = open('5.in').readlines()
+
+p1 = 0
+seat_ids = []
+for line in lines:
+    bin_str = ""
+    for ch in line.strip():
+        if ch in ['B', 'R']:
+            bin_str += '1'
+        elif ch in ['F', 'L']:
+            bin_str += '0'
+        else:
+            assert False, F"wrong character {ch}"
+    row = int(bin_str[:7], 2)
+    col = int(bin_str[7:], 2)
+    seat_id = row * 8 + col
+    seat_ids.append(seat_id)
+    p1 = max(p1, seat_id)
+p2 = None
+for id_ in range(p1, 0, -1):
+    if (id_ - 1) not in seat_ids:
+        p2 = id_ - 1
+        break
+
+print(p1)  # 896
+print(p2)  # 659
+
+stop = datetime.now()
+print("duration:", stop - start)
