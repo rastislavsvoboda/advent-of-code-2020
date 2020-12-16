a80327433fad757dc48b4383131b424e87c3b46c
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-16 17:06:17 +0100

    d16

---
+++ aoc_2020_d16-jonathan's modified.py
@@ -0,0 +1,123 @@
+from datetime import datetime
+from datetime import timedelta
+import functools
+import copy
+import re
+import time
+
+# .\get.ps1 16
+
+start = datetime.now()
+text = open('16.in').read()
+
+
+def get_data(text):
+    data = []
+    for grp in text.split('\n\n'):
+        data.append(grp.split('\n'))
+    return data
+
+
+def parse_rules(data):
+    rules = []
+    for line in data:
+        parts = line.split(':')
+        name = parts[0]
+        nums = [int(n) for n in re.findall(r'\d+', parts[1])]
+        assert len(nums) == 4
+        # print(name, nums)
+        rules.append(nums)
+    return rules
+
+
+def parse_your_ticket(lines):
+    assert lines[0] == "your ticket:"
+    ticket = [int(n) for n in lines[1].split(',')]
+    # print(ticket)
+    return ticket
+
+
+def parse_nearby_tickets(lines):
+    assert lines[0] == "nearby tickets:"
+    tickets = []
+    for line in lines[1:]:
+        if line:
+            ticket = [int(n) for n in line.split(',')]
+            tickets.append(ticket)
+    # print(tickets)
+    return tickets
+
+
+def valid_sum(rules, ticket):
+    s = 0
+    for n in ticket:
+        is_valid = False
+        for (min1, max1, min2, max2) in rules:
+            if min1 <= n <= max1 or min2 <= n <= max2:
+                # if at least num is in defined intervals, ticket is valid
+                is_valid = True
+                break
+        if not is_valid:
+            # add num that is not valid for any range
+            s += n
+    return s
+
+
+def solve1(text):
+    res = 0
+    data = get_data(text)
+    # print(data)
+    rules = parse_rules(data[0])
+    your_ticket = parse_your_ticket(data[1])
+    nearby_tickers = parse_nearby_tickets(data[2])
+    for t in nearby_tickers:
+        res += valid_sum(rules, t)
+    return res
+
+
+def get_assignments(rules, nearby_tickets):
+    l = len(rules)
+    possible_assignments = [[True for _ in range(l)] for _ in range(l)]
+    for t in nearby_tickets:
+        if valid_sum(rules, t) != 0:
+            continue  # skip invalid
+        for i, n in enumerate(t):
+            for j, (a, b, c, d) in enumerate(rules):
+                if not (a <= n <= b or c <= n <= d):
+                    possible_assignments[i][j] = False
+
+    MAP = [None for _ in range(l)]
+    USED = [False for _ in range(l)]
+    found = 0
+    while found < l:
+        for i in range(l):
+            valid_j = [
+                j for j in range(l)
+                if possible_assignments[i][j] and not USED[j]
+            ]
+            if len(valid_j) == 1:
+                MAP[i] = valid_j[0]
+                USED[valid_j[0]] = True
+                found += 1
+    return MAP
+
+
+def solve2(text):
+    data = get_data(text)
+    # print(data)
+    rules = parse_rules(data[0])
+    your_ticket = parse_your_ticket(data[1])
+    nearby_tickets = parse_nearby_tickets(data[2])
+    assignments = get_assignments(rules, nearby_tickets)
+    res = 1
+    for i, j in enumerate(assignments):
+        if j < 6:
+            res *= your_ticket[i]
+    return res
+
+
+print(solve1(text))  # 26980
+print(solve2(text))  # 3021381607403
+
+stop = datetime.now()
+print("duration:", stop - start)
