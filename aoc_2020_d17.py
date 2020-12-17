29b4435fb9f6ab349788defc146f0dd72f73e5ff
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-17 08:57:18 +0100

    d17 ref - point as tuple

--- aoc_2020_d17.py
+++ aoc_2020_d17.py
@@ -11,7 +11,8 @@ start = datetime.now()
 lines = open('17.in').readlines()
 
 
-def adj1(M, x, y, z):
+def adj1(M, p):
+    (x, y, z) = p
     cnt = 0
     offsets = [-1, 0, 1]
     for dx in offsets:
@@ -33,16 +34,18 @@ def evolve1(M, sizeX, sizeY, sizeZ):
     for z in range(-sizeZ, sizeZ + 1):
         for y in range(-sizeY, sizeY + 1):
             for x in range(-sizeX, sizeX + 1):
-                cube = M.get((x, y, z), '.')
-                cnt = adj1(M, x, y, z)
+                p = (x, y, z)
+                cube = M.get(p, '.')
+                cnt = adj1(M, p)
                 if cube == "#" and cnt not in [2, 3]:
-                    newM[(x, y, z)] = "."
+                    newM[p] = "."
                 elif cube == '.' and cnt == 3:
-                    newM[(x, y, z)] = "#"
+                    newM[p] = "#"
     return newM, sizeX, sizeY, sizeZ
 
 
-def adj2(M, x, y, z, w):
+def adj2(M, p):
+    (x, y, z, w) = p
     cnt = 0
     offsets = [-1, 0, 1]
     for dx in offsets:
@@ -51,8 +54,7 @@ def adj2(M, x, y, z, w):
                 for dw in offsets:
                     if dx == dy == dz == dw == 0:
                         continue
-                    p = (x + dx, y + dy, z + dz, w + dw)
-                    cube = M.get(p, '.')
+                    cube = M.get((x + dx, y + dy, z + dz, w + dw), '.')
                     if cube == "#":
                         cnt += 1
     return cnt
@@ -68,12 +70,13 @@ def evolve2(M, sizeX, sizeY, sizeZ, sizeW):
         for z in range(-sizeZ, sizeZ + 1):
             for y in range(-sizeY, sizeY + 1):
                 for x in range(-sizeX, sizeX + 1):
-                    cube = M.get((x, y, z, w), '.')
-                    cnt = adj2(M, x, y, z, w)
+                    p = (x, y, z, w)
+                    cube = M.get(p, '.')
+                    cnt = adj2(M, p)
                     if cube == "#" and cnt not in [2, 3]:
-                        newM[(x, y, z, w)] = "."
+                        newM[p] = "."
                     elif cube == '.' and cnt == 3:
-                        newM[(x, y, z, w)] = "#"
+                        newM[p] = "#"
     return newM, sizeX, sizeY, sizeZ, sizeW
 
 
@@ -91,7 +94,7 @@ def printM1(M, sizeX, sizeY, sizeZ):
         for y in range(-sizeY, sizeY + 1):
             l = ""
             for x in range(-sizeX, sizeX + 1):
-                cube = M.get((x, y, z),'.')
+                cube = M.get((x, y, z), '.')
                 l += cube
             print(l)
         print()
@@ -109,10 +112,9 @@ def solve1(lines):
             for x in range(-sizeX, sizeX + 1):
                 M[(x, y, z)] = '.'
 
-    z = 0
     for y, line in enumerate(lines):
         for x, c in enumerate(list(line.strip())):
-            M[(x - sizeX, y - sizeY, z)] = c
+            M[(x - sizeX, y - sizeY, 0)] = c
     # printM1(M, sizeX, sizeY, sizeZ)
     # print("----------------")
 
@@ -140,11 +142,9 @@ def solve2(lines):
                 for x in range(-sizeX, sizeX + 1):
                     M[(x, y, z, w)] = '.'
 
-    w = 0
-    z = 0
     for y, line in enumerate(lines):
         for x, c in enumerate(list(line.strip())):
-            M[(x - sizeX, y - sizeY, z, w)] = c
+            M[(x - sizeX, y - sizeY, 0, 0)] = c
 
     cycle = 0
     while cycle < 6:
