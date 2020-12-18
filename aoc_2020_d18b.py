7886133adb76c57e6f83baa45353752e51dfcf9f
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-18 21:44:42 +0100

    d18 - simpliified

---
+++ aoc_2020_d18b.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from datetime import timedelta
+from collections import defaultdict, deque
+import copy
+import re
+import time
+
+# .\get.ps1 18
+
+start = datetime.now()
+lines = open('18.in').readlines()
+
+
+def rec_eval(indx, line, p1):
+    q = deque()
+    # parse to queue
+    while indx < len(line):
+        c = line[indx]
+        if c == "(":
+            sub_eval, indx = rec_eval(indx + 1, line, p1)
+            q.append(sub_eval)
+        elif c == ")":
+            indx += 1
+            break
+        elif str.isdigit(c):
+            q.append(int(c))
+            indx += 1
+        elif c == '+' or c == '*':
+            q.append(c)
+            indx += 1
+        elif c == ' ':
+            indx += 1
+        else:
+            assert False, F"wrong char:{c}"
+    # evaluate
+    while len(q) >= 3:
+        n1 = q.popleft()
+        op = q.popleft()
+        n2 = q.popleft()
+        if op == "+":
+            q.appendleft(n1 + n2)
+        elif op == "*":
+            if p1:
+                q.appendleft(n1 * n2)
+            else:
+                if "+" not in q:
+                    q.appendleft(n1 * n2)
+                else:
+                    q_skipp = deque()
+                    while (op == "*"):
+                        q_skipp.append(n1)
+                        q_skipp.append(op)
+                        n1 = n2
+                        op = q.popleft()
+                        n2 = q.popleft()
+                    assert op == "+", "op should be +"
+                    q.appendleft(n1 + n2)
+                    while q_skipp:
+                        q.appendleft(q_skipp.pop())
+        else:
+            assert False, F"wrong op:{op}"
+    assert len(q) == 1, "q should have just 1 item left"
+    # get single val as result
+    res = q.pop()
+    return res, indx
+
+
+def evaluate(line, p1):
+    line = "(" + line + ")"
+    res, indx = rec_eval(0, line, p1)
+    assert indx == len(line), "should went through whole line"
+    return res
+
+
+def solve(lines, p1):
+    res = 0
+    for line in lines:
+        x = evaluate(line.strip(), p1)
+        # print(x)
+        res += x
+    return res
+
+
+print(solve(lines, True))  # 98621258158412
+print(solve(lines, False))  # 241216538527890
+
+stop = datetime.now()
+print("duration:", stop - start)
