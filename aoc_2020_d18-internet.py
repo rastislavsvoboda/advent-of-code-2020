ed11558405e81676e0d7ee6e5455f321a721df26
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-18 17:31:10 +0100

    d18 other improved

--- aoc_2020_d18-internet.py
+++ aoc_2020_d18-internet.py
@@ -11,34 +11,25 @@ start = datetime.now()
 lines = open('18.in').readlines()
 
 
-class num1(int):
+class num(int):
     def __sub__(self, b):
-        return num1(int(self) * int(b))
+        return num(int(self) * int(b))
 
     def __add__(self, b):
-        return num1(int(self) + int(b))
-
-
-class num2(int):
-    def __sub__(self, b):
-        return num2(int(self) * int(b))
+        return num(int(self) + int(b))
 
     def __mul__(self, b):
-        return num2(int(self) + int(b))
+        return num(int(self) + int(b))
 
 
 def solve(lines, p1):
     res = 0
     for line in lines:
         line = line.strip()
-        if p1:
-            line = line.replace('*', '-')
-            line = re.sub('(\d+)', r'num1(\1)', line)
-        else:
-            line = line.replace('*', '-')
+        line = line.replace('*', '-')
+        if not p1:
             line = line.replace('+', '*')
-            line = re.sub('(\d+)', r'num2(\1)', line)
-
+        line = re.sub('(\d+)', r'num(\1)', line)
         x = eval(line)
         # print(x)
         res += x
