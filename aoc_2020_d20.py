a72db9ba0785f7a3815ba7f97443b8b393a174c4
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-20 21:33:02 +0100

    d20 p2

--- aoc_2020_d20.py
+++ aoc_2020_d20.py
@@ -10,13 +10,13 @@ import time
 # .\get.ps1 20
 
 start = datetime.now()
-lines = open('20.in0').readlines()
+lines = open('20.in').readlines()
 
 
 def print_g(g):
-    for r in range(10):
+    for r in range(len(g)):
         print("".join(g[r]))
-    print("----------")
+    print(len(g) * "-")
 
 
 def get_combs(name, g):
@@ -32,24 +32,26 @@ def get_combs(name, g):
 
 
 def rotate_grdR(g):
+    LG = len(g)
     old = copy.deepcopy(g)
-    for r in range(10):
-        for c in range(10):
-            g[c][9 - r] = old[r][c]
+    for r in range(LG):
+        for c in range(LG):
+            g[c][LG - 1 - r] = old[r][c]
 
 
 def flip_grdH(g):
+    LG = len(g)
     old = copy.deepcopy(g)
-    for r in range(10):
-        for c in range(10):
-            g[9 - r][c] = old[r][c]
+    for r in range(LG):
+        for c in range(LG):
+            g[LG - 1 - r][c] = old[r][c]
 
 
-def flip_grdV(g):
-    old = copy.deepcopy(g)
-    for r in range(10):
-        for c in range(10):
-            g[r][9 - c] = old[r][c]
+# def flip_grdV(g):
+#     old = copy.deepcopy(g)
+#     for r in range(10):
+#         for c in range(10):
+#             g[r][9 - c] = old[r][c]
 
 
 def parse_grid(lines):
@@ -85,11 +87,12 @@ for i in range(0, len(lines), 12):
     grd_lines = lines[i:i + 11]
     name, grid = parse_grid(grd_lines)
     GRIDS[name] = grid
+    # print_g(grid)
     COMBS[name] = get_combs(name, grid)
     IDX.append(name)
 
 size = int(math.sqrt(len(IDX)))
-print(size)
+# print(size)
 res_grd = [[None for _ in range(size)] for _ in range(size)]
 seen = set()
 
@@ -106,6 +109,7 @@ def place(n):
 
         cmbs = COMBS[name]
         for nm, cg in cmbs:
+
             if y > 0:
                 _, up_gr = res_grd[y - 1][x]
                 if not chck_grid_h(up_gr, cg):
@@ -131,8 +135,90 @@ a = gr[0][0][0][:4]
 b = gr[0][size - 1][0][:4]
 c = gr[size - 1][0][0][:4]
 d = gr[size - 1][size - 1][0][:4]
-print(a, b, c, d)
+# print(a, b, c, d)
 print(int(a) * int(b) * int(c) * int(d))  # 2699020245973
+
+# assert False
+tileSize = 10
+imageSize = tileSize-2
+
+final = [['?' for _ in range(imageSize*size)] for _ in range(imageSize*size)]
+assert(len(final) == len(final[0]))
+
+for y, row in enumerate(gr):
+    for x,ng in enumerate(row):
+        name, grid = ng
+        # print_g(grid)
+        for r in range(1,tileSize-1):
+            for c in range(1,tileSize-1):
+                final[y*imageSize+r-1][x*imageSize+c-1] = grid[r][c]
+        # print(grid)
+
+
+# print_g(final)
+
+monster = '''                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   '''.splitlines()
+
+monst_grd = []
+for r, line in enumerate(monster):
+    row = [] 
+    for c, ch in enumerate(line):
+        row.append(ch)
+    monst_grd.append(row)
+
+FINAL_R = len(final)
+FINAL_C = len(final[0])
+
+MON_R = len(monst_grd)
+MON_C = len(monst_grd[0])
+
+final_cmb = get_combs("final", final)
+
+# print_g(monst_grd)
+
+sizes = [-1 for _ in range(len(final_cmb))]
+hasMonsters = [ 0 for _ in range(len(final_cmb)) ]
+drawed = [ None for _ in range(len(final_cmb)) ]
+
+for i,fcmb in enumerate(final_cmb):
+    name, f = fcmb
+    for rr in range(0, FINAL_R-MON_R+1):
+        for cc in range(0, FINAL_C-MON_C+1):
+            all_match = True
+            for r in range(MON_R):
+                for c in range(MON_C):
+                    if monst_grd[r][c] == "#":
+                        if f[rr+r][cc+c] != "#":
+                            all_match = False
+            if all_match:
+                hasMonsters[i] += 1
+                for r in range(MON_R):
+                    for c in range(MON_C):
+                        if monst_grd[r][c] == "#":
+                            f[rr+r][cc+c] = "O"
+                drawed[i] = f
+
+
+# print(hasMonsters)
+for i,mon_cnt in enumerate(hasMonsters):
+    if mon_cnt > 0:
+        # print_g(drawed[i])
+        cnt = 0
+        for r, row in enumerate(drawed[i]):
+            for c, ch in enumerate(row):
+                if ch == "#":
+                    cnt+=1
+        print(cnt)
+
+
+
+                
+
+
+
+
 
 
 
