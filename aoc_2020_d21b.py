147bdcb853b009ba927109b1fa829ec21829d6a8
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-21 18:47:13 +0100

    d21 b (original)

---
+++ aoc_2020_d21b.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+from collections import defaultdict
+
+# .\get.ps1 21
+
+start = datetime.now()
+lines = open('21.in').readlines()
+
+# part 1
+allergens = set()
+possible = dict()
+counts = defaultdict(int)
+for line in lines:
+    left, right = line.strip().split('(contains ')
+    Is = left.split()  # Ingredients
+    As = right[:-1].split(', ')  # Allergens
+    allergens |= set(As)
+    for a in As:
+        if a not in possible:
+            possible[a] = set(Is)
+        else:
+            possible[a] &= set(Is)
+    for i in Is:
+        counts[i] += 1
+
+# print("allergens cnt", len(allergens))
+
+ASSIGN = {}
+
+while len(ASSIGN) < len(allergens):
+    # my original reducing algorithm
+    found_ingrd = None
+    for allerg, ingrds in possible.items():
+        if len(ingrds) == 1:
+            found_ingrd = ingrds.pop()
+            ASSIGN[found_ingrd] = allerg
+            break
+    assert found_ingrd, "assuming each round eliminates one ingredient"
+    for ingrds in possible.values():
+        ingrds.discard(found_ingrd)
+    del possible[allerg]
+    # can be simplified to:
+    # for a in allergens:
+    #     Is = [i for i in possible[a] if i not in ASSIGN]
+    #     if len(Is) == 1:
+    #         ASSIGN[Is[0]] = a
+    #         break
+
+# print(ASSIGN)
+res1 = 0
+for ingrd, cnt in counts.items():
+    if ingrd not in ASSIGN:
+        res1 += cnt
+
+print("part1:", res1)  # 2874
+
+# part 2
+sorted_by_allerg = sorted(ASSIGN.items(), key=lambda pair: pair[1])
+res2 = ",".join(map(lambda pair: pair[0], sorted_by_allerg))
+print("part2:", res2)  # gfvrr,ndkkq,jxcxh,bthjz,sgzr,mbkbn,pkkg,mjbtz
+
+stop = datetime.now()
+print("duration:", stop - start)
