dadbb2c203351f37dcca93ccb7770b947c100c7c
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-21 14:58:30 +0100

    d21 - typo

--- aoc_2020_d21.py
+++ aoc_2020_d21.py
@@ -7,7 +7,7 @@ from collections import defaultdict, deque
 start = datetime.now()
 lines = open('21.in').readlines()
 
-alergens = set()
+allergens = set()
 possible = dict()
 counts = defaultdict(int)
 for line in lines:
@@ -15,27 +15,27 @@ for line in lines:
     st_br = str.index(line, "(")
     ed_br = -1
     assert line[ed_br] == ")"
-    alerg_parts = line[st_br + len("contains ") + 1:ed_br].split(", ")
+    allerg_parts = line[st_br + len("contains ") + 1:ed_br].split(", ")
     ingrd_parts = line[:st_br].split()
-    for alerg in alerg_parts:
-        alergens.add(alerg)
-        if alerg not in possible:
-            possible[alerg] = set(ingrd_parts)
+    for allerg in allerg_parts:
+        allergens.add(allerg)
+        if allerg not in possible:
+            possible[allerg] = set(ingrd_parts)
         else:
-            possible[alerg] &= set(ingrd_parts)
+            possible[allerg] &= set(ingrd_parts)
     for ingrd in ingrd_parts:
         counts[ingrd] += 1
 
-# print("alergens cnt", len(alergens))
+# print("allergens cnt", len(allergens))
 
 ASSIGN = {}
 
-while len(ASSIGN) < len(alergens):
+while len(ASSIGN) < len(allergens):
     found_ingrd = None
-    for alerg, ingrds in possible.items():
+    for allerg, ingrds in possible.items():
         if len(ingrds) == 1:
             found_ingrd = ingrds.pop()
-            ASSIGN[found_ingrd] = alerg
+            ASSIGN[found_ingrd] = allerg
             break
     assert found_ingrd
     for ingrds in possible.values():
@@ -50,8 +50,8 @@ for ingrd, cnt in counts.items():
 
 print("part1:", res1)  # 2874
 
-sorted_by_alerg = sorted(ASSIGN.items(), key=lambda pair: pair[1])
-res2 = ",".join(map(lambda pair: pair[0], sorted_by_alerg))
+sorted_by_allerg = sorted(ASSIGN.items(), key=lambda pair: pair[1])
+res2 = ",".join(map(lambda pair: pair[0], sorted_by_allerg))
 print("part2:", res2)  # gfvrr,ndkkq,jxcxh,bthjz,sgzr,mbkbn,pkkg,mjbtz
 
 stop = datetime.now()
