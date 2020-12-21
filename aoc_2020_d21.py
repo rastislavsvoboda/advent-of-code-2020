2baf3980971333b9b4ffcf2881a68db35496c8bb
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-21 11:03:03 +0100

    d21 refactored

--- aoc_2020_d21.py
+++ aoc_2020_d21.py
@@ -1,10 +1,6 @@
 from datetime import datetime
 from datetime import timedelta
 from collections import defaultdict, deque
-import functools
-import copy
-import re
-import time
 
 # .\get.ps1 21
 
@@ -12,7 +8,7 @@ start = datetime.now()
 lines = open('21.in').readlines()
 
 alergens = set()
-possible = defaultdict(list)
+possible = dict()
 counts = defaultdict(int)
 for line in lines:
     line = line.strip()
@@ -23,7 +19,10 @@ for line in lines:
     ingrd_parts = line[:st_br].split()
     for alerg in alerg_parts:
         alergens.add(alerg)
-        possible[alerg].append(set(ingrd_parts))
+        if alerg not in possible:
+            possible[alerg] = set(ingrd_parts)
+        else:
+            possible[alerg] &= set(ingrd_parts)
     for ingrd in ingrd_parts:
         counts[ingrd] += 1
 
@@ -31,21 +30,17 @@ for line in lines:
 
 ASSIGN = {}
 
-for alerg, ingrds in possible.items():
-    possible[alerg] = functools.reduce(lambda a, b: a & b, ingrds)
-
 while len(ASSIGN) < len(alergens):
-    found = None
+    found_ingrd = None
     for alerg, ingrds in possible.items():
         if len(ingrds) == 1:
-            ingrd = ingrds.pop()
-            ASSIGN[ingrd] = alerg
-            found = ingrd
+            found_ingrd = ingrds.pop()
+            ASSIGN[found_ingrd] = alerg
             break
-    assert found
+    assert found_ingrd
     for ingrds in possible.values():
-        ingrds.discard(found)
-    del possible[ASSIGN[found]]
+        ingrds.discard(found_ingrd)
+    del possible[ASSIGN[found_ingrd]]
 
 # print(ASSIGN)
 res1 = 0
@@ -56,11 +51,7 @@ for ingrd, cnt in counts.items():
 print("part1:", res1)  # 2874
 
 sorted_by_alerg = sorted(ASSIGN.items(), key=lambda pair: pair[1])
-# print(sorted_by_alerg)
-ingreds = list(map(lambda pair: pair[0], sorted_by_alerg))
-# print(ingreds)
-
-res2 = ",".join(ingreds)
+res2 = ",".join(map(lambda pair: pair[0], sorted_by_alerg))
 print("part2:", res2)  # gfvrr,ndkkq,jxcxh,bthjz,sgzr,mbkbn,pkkg,mjbtz
 
 stop = datetime.now()
