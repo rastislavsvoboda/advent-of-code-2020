4ebd14d9760571d625172b8f513f0f1ad90eee21
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-24 13:41:22 +0100

    d23 renamed

--- aoc_2020_d23c.py
+++ aoc_2020_d23c.py
@@ -15,41 +15,41 @@ line = lines[0]
 def solve1(line):
     ns = list(map(int, list(line.strip())))
     l = len(ns)
-    nums = [None] * (l+1)
+    pointers = [None] * (l+1)
     
     s = ns[0]
     for x in ns[1:]:
-        nums[s] = x
+        pointers[s] = x
         s = x
-    nums[s] = ns[0]
+    pointers[s] = ns[0]
 
     s_v = ns[0]
     
     for r in range(100):    
-        s_next = nums[s_v]
+        s_next = pointers[s_v]
         p1 = s_next
-        p1_next = nums[p1]
+        p1_next = pointers[p1]
         p2 = p1_next
-        p2_next = nums[p2]
+        p2_next = pointers[p2]
         p3 = p2_next
-        p3_next = nums[p3]
+        p3_next = pointers[p3]
 
         dst = s_v
         while dst == s_v or dst == p1 or dst == p2 or dst == p3:
             dst = dst - 1 if dst > 1 else l
 
-        dst_next = nums[dst]
-        nums[s_v] = p3_next
-        nums[dst] = p1
-        nums[p3] = dst_next
+        dst_next = pointers[dst]
+        pointers[s_v] = p3_next
+        pointers[dst] = p1
+        pointers[p3] = dst_next
 
         s_v = p3_next
 
     res = ""
-    x = nums[1]
+    x = pointers[1]
     while x != 1:
         res += str(x)
-        x = nums[x]
+        x = pointers[x]
 
     return res
 
@@ -62,39 +62,39 @@ def solve2(line):
         ns.append(i)
 
     l = len(ns)
-    nums = [None] * (l+1)
+    pointers = [None] * (l+1)
 
     s = ns[0]
     for x in ns[1:]:
-        nums[s] = x
+        pointers[s] = x
         s = x
-    nums[s] = ns[0]
+    pointers[s] = ns[0]
 
     s_v = ns[0]
     
     for r in range(10_000_000):    
-        s_next = nums[s_v]
+        s_next = pointers[s_v]
         p1 = s_next
-        p1_next = nums[p1]
+        p1_next = pointers[p1]
         p2 = p1_next
-        p2_next = nums[p2]
+        p2_next = pointers[p2]
         p3 = p2_next
-        p3_next = nums[p3]
+        p3_next = pointers[p3]
 
         dst = s_v
         while dst == s_v or dst == p1 or dst == p2 or dst == p3:
             dst = dst - 1 if dst > 1 else l
 
-        dst_next = nums[dst]
-        nums[s_v] = p3_next
-        nums[dst] = p1
-        nums[p3] = dst_next
+        dst_next = pointers[dst]
+        pointers[s_v] = p3_next
+        pointers[dst] = p1
+        pointers[p3] = dst_next
 
         s_v = p3_next
 
 
-    a = nums[1]
-    b = nums[a]
+    a = pointers[1]
+    b = pointers[a]
     res = a * b
     return res
 
