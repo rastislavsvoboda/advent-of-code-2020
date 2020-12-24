4fcd4f18020a7772c252029d2397429f1aa1dd55
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-24 13:54:40 +0100

    d23

--- aoc_2020_d23c.py
+++ aoc_2020_d23c.py
@@ -1,9 +1,4 @@
 from datetime import datetime
-from datetime import timedelta
-from collections import defaultdict, deque
-import copy
-import re
-import time
 
 # .\get.ps1 23
 
@@ -12,95 +7,68 @@ lines = open('23.in').readlines()
 line = lines[0]
 
 
-def solve1(line):
-    ns = list(map(int, list(line.strip())))
-    l = len(ns)
-    pointers = [None] * (l+1)
-    
-    s = ns[0]
-    for x in ns[1:]:
-        pointers[s] = x
-        s = x
-    pointers[s] = ns[0]
+def solve(line, part1):
+    rounds = 100 if part1 else 10_000_000
 
-    s_v = ns[0]
-    
-    for r in range(100):    
-        s_next = pointers[s_v]
-        p1 = s_next
-        p1_next = pointers[p1]
-        p2 = p1_next
-        p2_next = pointers[p2]
-        p3 = p2_next
-        p3_next = pointers[p3]
-
-        dst = s_v
-        while dst == s_v or dst == p1 or dst == p2 or dst == p3:
-            dst = dst - 1 if dst > 1 else l
-
-        dst_next = pointers[dst]
-        pointers[s_v] = p3_next
-        pointers[dst] = p1
-        pointers[p3] = dst_next
-
-        s_v = p3_next
-
-    res = ""
-    x = pointers[1]
-    while x != 1:
-        res += str(x)
-        x = pointers[x]
-
-    return res
-
-
-def solve2(line):
     ns = list(map(int, list(line.strip())))
 
-    highest = max(ns)
-    for i in range(highest + 1, 1_000_000 + 1):
-        ns.append(i)
+    if not part1:
+        highest = max(ns)
+        for i in range(highest + 1, 1_000_000 + 1):
+            ns.append(i)
 
     l = len(ns)
-    pointers = [None] * (l+1)
+
+    # nexts stores "pointers"
+    # index is the value itself
+    # next[index] is the value of then following next item
+    # 0 index is not used, 0 is not in the list
+    nexts = [None] * (l+1)
 
     s = ns[0]
     for x in ns[1:]:
-        pointers[s] = x
+        nexts[s] = x
         s = x
-    pointers[s] = ns[0]
+    nexts[s] = ns[0]
 
-    s_v = ns[0]
+    selected = ns[0]
     
-    for r in range(10_000_000):    
-        s_next = pointers[s_v]
+    for r in range(rounds):    
+        s_next = nexts[selected]
         p1 = s_next
-        p1_next = pointers[p1]
+        p1_next = nexts[p1]
         p2 = p1_next
-        p2_next = pointers[p2]
+        p2_next = nexts[p2]
         p3 = p2_next
-        p3_next = pointers[p3]
+        p3_next = nexts[p3]
 
-        dst = s_v
-        while dst == s_v or dst == p1 or dst == p2 or dst == p3:
+        dst = selected
+        while dst == selected or dst == p1 or dst == p2 or dst == p3:
             dst = dst - 1 if dst > 1 else l
 
-        dst_next = pointers[dst]
-        pointers[s_v] = p3_next
-        pointers[dst] = p1
-        pointers[p3] = dst_next
+        dst_next = nexts[dst]
+        nexts[selected] = p3_next
+        nexts[dst] = p1
+        nexts[p3] = dst_next
 
-        s_v = p3_next
+        selected = p3_next
 
 
-    a = pointers[1]
-    b = pointers[a]
-    res = a * b
+    if part1:
+        res = ""
+        x = nexts[1]
+        while x != 1:
+            res += str(x)
+            x = nexts[x]
+    else:
+        a = nexts[1]
+        b = nexts[a]
+        res = a * b
     return res
 
 
-print(solve1(line))  # 26354798
-print(solve2(line))  # 166298218695
+print(solve(line, True))  # 26354798
+print(solve(line, False))  # 166298218695
 
 stop = datetime.now()
 print("duration:", stop - start)
