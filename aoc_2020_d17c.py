435aea4cc796f733a760ca2b562f10569c47f9c4
Rastislav Svoboda <Rastislav.Svoboda@gmail.com>
2020-12-25 17:01:43 +0100

    d17

--- aoc_2020_d17c.py
+++ aoc_2020_d17c.py
@@ -11,11 +11,37 @@ start = datetime.now()
 lines = open('17.in').readlines()
 
 
+def get_adjs3D(p):
+    (x, y, z) = p
+    offsets = [-1, 0, 1]
+    adjs = []
+    for dx in offsets:
+        for dy in offsets:
+            for dz in offsets:
+                if dx == dy == dz == 0:
+                    continue
+                adjs.append((x + dx, y + dy, z + dz))
+    return adjs
+
+
+def get_adjs4D(p):
+    (x, y, z, w) = p
+    offsets = [-1, 0, 1]
+    adjs = []
+    for dx in offsets:
+        for dy in offsets:
+            for dz in offsets:
+                for dw in offsets:
+                    if dx == dy == dz == dw == 0:
+                        continue
+                    adjs.append((x + dx, y + dy, z + dz, w + dw))
+    return adjs
+
+
 def solve1(lines):
     M = set()
     sizeX = len(lines[0]) // 2
     sizeY = len(lines) // 2
-    offsets = [-1, 0, 1]
 
     for y, line in enumerate(lines):
         for x, c in enumerate(list(line.strip())):
@@ -26,29 +52,21 @@ def solve1(lines):
         newM = set()
         to_check = set()
 
-        for (x, y, z) in M:
-            to_check.add((x, y, z))
-            for dx in offsets:
-                for dy in offsets:
-                    for dz in offsets:
-                        if dx == dy == dz == 0:
-                            continue
-                        to_check.add((x + dx, y + dy, z + dz))
+        for p in M:
+            to_check.add(p)
+            for adj in get_adjs3D(p):
+                to_check.add(adj)
 
-        for (x, y, z) in to_check:
+        for p in to_check:
             cnt = 0
-            for dx in offsets:
-                for dy in offsets:
-                    for dz in offsets:
-                        if dx == dy == dz == 0:
-                            continue
-                        if (x + dx, y + dy, z + dz) in M:
-                            cnt += 1
-            is_on = (x, y, z) in M
+            for adj in get_adjs3D(p):
+                if adj in M:
+                    cnt += 1
+            is_on = p in M
             if is_on and cnt in [2, 3]:
-                newM.add((x, y, z))
+                newM.add(p)
             elif not is_on and cnt == 3:
-                newM.add((x, y, z))
+                newM.add(p)
         M = newM
     res = len(M)
     return res
@@ -58,7 +76,6 @@ def solve2(lines):
     M = set()
     sizeX = len(lines[0]) // 2
     sizeY = len(lines) // 2
-    sizeZ = 0
     offsets = [-1, 0, 1]
 
     for y, line in enumerate(lines):
@@ -70,31 +87,21 @@ def solve2(lines):
         newM = set()
         to_check = set()
 
-        for (x, y, z, w) in M:
-            to_check.add((x, y, z, w))
-            for dx in offsets:
-                for dy in offsets:
-                    for dz in offsets:
-                        for dw in offsets:
-                            if dx == dy == dz == dw == 0:
-                                continue
-                            to_check.add((x + dx, y + dy, z + dz, w + dw))
+        for p in M:
+            to_check.add(p)
+            for adj in get_adjs4D(p):
+                to_check.add(adj)
 
-        for (x, y, z, w) in to_check:
+        for p in to_check:
             cnt = 0
-            for dx in offsets:
-                for dy in offsets:
-                    for dz in offsets:
-                        for dw in offsets:
-                            if dx == dy == dz == dw == 0:
-                                continue
-                            if (x + dx, y + dy, z + dz, w + dw) in M:
-                                cnt += 1
-            is_on = (x, y, z, w) in M
+            for adj in get_adjs4D(p):
+                if adj in M:
+                    cnt += 1
+            is_on = p in M
             if is_on and cnt in [2, 3]:
-                newM.add((x, y, z, w))
+                newM.add(p)
             elif not is_on and cnt == 3:
-                newM.add((x, y, z, w))
+                newM.add(p)
         M = newM
     res = len(M)
     return res
